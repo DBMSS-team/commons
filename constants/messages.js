54f6f58b59d9e9c135bf5238f5b41c9f35eb377b
I355102 <sahil.kumar@sap.com>
2020-08-27 22:12:44 +0530

    alligned few changes

--- constants/messages.js
+++ constants/messages.js
@@ -1,8 +1,9 @@
-const responseMessages = {
+module.exports = {
 	NO_DATA_FOUND: 'No Data found',
 	SUCCESS_MESSAGE: 'Data retrieved  Successfully',
+	REFRESH_TOKEN_NOT_FOUND: 'Refresh token not found',
+	NO_USER_FOUND: 'No user found with this username',
+	INCORRECT_PASSWORD: 'Incorrect Password',
+	MAX_ATTEMPTS_EXCEEDED: 'Maximum attempts for password exceeded',
+	USER_LOCKED: 'User is locked',
 };
-
-const errorMessages = {};
-
-module.exports = responseMessages;
